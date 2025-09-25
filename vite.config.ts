158421069249c32b919994e6a5ab842027b8a798
Lars <lars.dehairs@gmail.com>
2025-09-25 19:23:22 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/Static-Link-Web/', // <--- add this line, replace with your GitHub repo name
+        base: '/Static-Link-Web/', // <--- add this line, replace with your Github repo name
       server: {
         port: 3000,
         host: '0.0.0.0',
